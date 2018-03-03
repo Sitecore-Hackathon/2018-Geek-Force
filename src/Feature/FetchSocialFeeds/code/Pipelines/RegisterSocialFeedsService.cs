8c1c1ce434464a86f24b55cf9bd02bead0e4820b
ufaqeeh <ufaqeeh@live.com>
2018-03-03 22:44:57 +0500

    [update] revert back to IOC Processor

--- src/Feature/FetchSocialFeeds/code/Pipelines/RegisterSocialFeedsService.cs
+++ src/Feature/FetchSocialFeeds/code/Pipelines/RegisterSocialFeedsService.cs
@@ -2,6 +2,7 @@
 using Hackathon.SocialWall.Feature.FetchSocialFeeds.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Sitecore.DependencyInjection;
+using Sitecore.XA.Foundation.IOC.Pipelines.IOC;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,20 @@ using System.Web;
 
 namespace Hackathon.SocialWall.Feature.FetchSocialFeeds.Pipelines
 {
-    public class RegisterSocialFeedsService : IServicesConfigurator
+    public class RegisterSocialFeedsService : IocProcessor
     {
-        public void Configure(IServiceCollection serviceCollection)
+        public override void Process(IocArgs args)
         {
-            serviceCollection.AddTransient<ISocialFeedsService, SocialFeedsService>();
-            serviceCollection.AddTransient<ISocialFeedsRepository,SocialFeedsRepository>();
-            
-        }
+            args.ServiceCollection.AddTransient<ISocialFeedsService, SocialFeedsService>();
+            args.ServiceCollection.AddTransient<ISocialFeedsRepository,SocialFeedsRepository>();
+
+        } //: IServicesConfigurator
+          //{
+          //    public void Configure(IServiceCollection serviceCollection)
+          //    {
+          //        serviceCollection.AddTransient<ISocialFeedsService, SocialFeedsService>();
+          //        serviceCollection.AddTransient<ISocialFeedsRepository,SocialFeedsRepository>();
+
+        //    }
     }
 }
