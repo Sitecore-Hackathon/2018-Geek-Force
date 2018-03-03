10b049abf548be12f2a90738eaf1b85a4538e74d
ufaqeeh <ufaqeeh@live.com>
2018-03-03 21:46:27 +0500

    controller added

---
+++ src/Feature/FetchSocialFeeds/code/Controllers/SocialWallController.cs
@@ -0,0 +1,28 @@
+﻿using Hackathon.SocialWall.Feature.FetchSocialFeeds.Repositories;
+using Hackathon.SocialWall.Feature.FetchSocialFeeds.Services;
+using Sitecore.XA.Foundation.Mvc.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hackathon.SocialWall.Feature.FetchSocialFeeds.Controllers
+{
+    public class SocialWallController : StandardController
+    {
+        // GET: Forms
+        protected readonly ISocialFeedsService SocialFeedsService;
+
+        public SocialWallController(ISocialFeedsService socialFeedsService)
+        {
+            this.SocialFeedsService = socialFeedsService;
+        }
+        // GET: SocialWall
+        public ActionResult Index()
+        {
+            SocialFeedsService.AddPostsByHashTags();
+            return View();
+        }
+    }
+}
